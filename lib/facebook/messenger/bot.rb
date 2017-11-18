af636bbf7d768efc57fa5053085f40e11abe4df2
Caio <caio.dscamara@gmail.com>
2017-11-18 03:36:09 -0200

    Gem client para Messenger Bots.

--- lib/facebook/messenger/bot.rb
+++ lib/facebook/messenger/bot.rb
@@ -39,7 +39,7 @@ module Facebook
 
           Facebook::Messenger::Bot::ErrorParser.raise_errors_from(response)
 
-          response.body
+          #response.body
         end
 
         # Register a hook for the given event.
